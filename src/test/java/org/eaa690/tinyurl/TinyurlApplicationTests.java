ff0520946a6c9aaaee47b9c953d1419ad6edf6ec
Brian Michael <bsmichae42@yahoo.com>
2022-02-26 08:13:36 -0500

    Initial checkin

---
+++ src/test/java/org/eaa690/tinyurl/TinyurlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eaa690.tinyurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
